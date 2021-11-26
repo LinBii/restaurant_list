abf4ab19699d8c8f51c5d26b8d47c8ceac438474
LinBii <aa68968@gmail.com>
2021-11-26 08:46:11 +0800

    Add toLowerCase for category searching

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.category.includes(keyword.toLowerCase())||restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    return restaurant.category.toLowerCase.includes(keyword.toLowerCase())||restaurant.name.toLowerCase().includes(keyword.toLowerCase())
   })
   res.render('index', { restaurants, keyword })
 })
