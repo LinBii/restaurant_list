6a12402df3d9fd338733419e375e709457953955
LinBii <aa68968@gmail.com>
2021-12-08 20:12:49 +0800

    refactor: modify search routes

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -11,6 +11,20 @@ router.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+// 搜尋特定餐廳
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase()
 
+  Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      const filteredData = restaurants.filter(
+        data =>
+          data.category.toLowerCase().includes(keyword) || data.name.toLowerCase().includes(keyword)
+      )
+      res.render('index', { restaurants: filteredData, keyword })
+    })
+    .catch(error => console.log(error))
+})
 
 module.exports = router
