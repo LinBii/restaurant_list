0239dccf065e751721aedab5b456ccbf5355ffb3
LinBii <aa68968@gmail.com>
2022-01-08 21:22:14 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-// load method-override
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const routes = require('./routes')
 require('./config/mongoose')
 
@@ -24,6 +24,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 
 app.use(routes)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -11,6 +11,7 @@ router.post('/login', passport.authenticate('local', {
 }))
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 router.get('/register', (req, res) => {
@@ -18,10 +19,26 @@ router.get('/register', (req, res) => {
 })
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: '必須填寫Email、密碼及確認密碼！' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ email }).then(user => {
     if (user) {
-      console.log('User already exists.')
-      res.render('register', {
+      errors.push({ message: '這個 Email 已經註冊過了！ '})
+      return res.render('register', {
         name,
         email,
         password,
